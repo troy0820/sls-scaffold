6df7bc2462906ce5e6ce09b150260eccf81fb299
Troy Connor <troy.connor@yahoo.com>
2017-01-10 17:52:19 -0500

    [fixit]Added function for seeclickfixit api results

--- handlers/hello.js
+++ handlers/hello.js
@@ -1,14 +1,14 @@
 'use strict';
 
 module.exports.hello = (event, context, callback) => {
-	  const response = {
-		      statusCode: 200,
-		          body: JSON.stringify({
-				        message: 'Go Serverless v1.0! Your function executed successfully!',
-					      event: event
-						          })
-		        };
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: 'Go Serverless v1.0! Your function executed successfully!',
+      event: event
+    })
+  };
 
-	    callback(null, response);
+  callback(null, response);
 
 };
