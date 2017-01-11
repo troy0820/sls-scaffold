cac2e0a3e8baa7842a7b409c6b1b7ce7e4fe2ad8
Troy Connor <troy.connor@yahoo.com>
2017-01-11 09:41:54 -0500

    [TODO]Added TODO to deploy function

--- handlers/fixit.js
+++ handlers/fixit.js
@@ -1,23 +1,23 @@
-'use strict';
+e strict';
 
 const rp = require('request-promise');
 const _ = require('lodash');
 
 module.exports.fixit = (event, context, callback) => {
-
-rp('https://seeclickfix.com/api/v2/issues?place_url=hampton&state=VA&per_page=20&page=1')
-  .then((data) => {
-    const list = JSON.parse(data).issues;
-    const summaries = _.map(list, 'summary');
-    const response = {
-      statusCode: 200,
-      body: JSON.stringify({
-      issues: _.union(summaries) || 'There are no issues in this city'
-      })
-    };
-  callback(null, response);
-}).
-catch((err) =>{
-   callback(err, err);
-  });
-};
+	
+	rp('https://seeclickfix.com/api/v2/issues?place_url=hampton&state=VA&per_page=20&page=1')
+		  .then((data) => {
+			      const list = JSON.parse(data).issues;
+			          const summaries = _.map(list, 'summary');
+				      const response = {
+					            statusCode: 200,
+						          body: JSON.stringify({
+								        issues: _.union(summaries) || 'There are no issues in this city'
+										      })
+						        };
+				        callback(null, response);
+		  }).
+	catch((err) =>{
+		   callback(err, err);
+		     });
+};use strict';
