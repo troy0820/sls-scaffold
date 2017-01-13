a8506cbe479975876c03c0afdbd7f1f6416a8074
Troy Connor <troy0820@gmail.com>
2017-01-13 16:15:51 -0500

    [handler]Changed fixit handler city to norfolk

--- handlers/fixit.js
+++ handlers/fixit.js
@@ -4,7 +4,7 @@ const rp = require('request-promise');
 //TODO: Don't forget to deploy this function for the latest
 module.exports.fixit = (event, context, callback) => {
 
-rp('https://seeclickfix.com/api/v2/issues?place_url=portsmouth&state=VA&per_page=20&page=1')
+rp('https://seeclickfix.com/api/v2/issues?place_url=norfolk&state=VA&per_page=20&page=1')
   .then((data) => {
     const response = {
       statusCode: 200,
