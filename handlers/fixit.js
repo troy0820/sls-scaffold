4140f652396135a456bb7f5db31a85dc4f300d7d
Troy Connor <troy0820@gmail.com>
2017-01-15 16:16:16 -0500

    [handler]Created function to return all unique issue summaries

--- handlers/fixit.js
+++ handlers/fixit.js
@@ -1,15 +1,18 @@
 'use strict';
 
 const rp = require('request-promise');
+const _ = require('lodash');
 //TODO: Don't forget to deploy this function for the latest
 module.exports.fixit = (event, context, callback) => {
 
 rp('https://seeclickfix.com/api/v2/issues?place_url=norfolk&state=VA&per_page=20&page=1')
   .then((data) => {
+    const list = JSON.parse(data).issues;
+    const summaries = _.map(list, 'summary');
     const response = {
       statusCode: 200,
       body: {
-      issues: JSON.parse(data).issues,
+      issues: _.union(summaries),
       },
     };
   callback(null, response);
