b3573698997b4d37e10f970af90c5e3841dd9cfe
Troy Connor <troy0820@gmail.com>
2017-01-17 17:08:32 -0500

    [handler]Changed city and guard message on issues

--- handlers/fixit.js
+++ handlers/fixit.js
@@ -5,14 +5,14 @@ const _ = require('lodash');
 
 module.exports.fixit = (event, context, callback) => {
 
-rp('https://seeclickfix.com/api/v2/issues?place_url=norfolk&state=VA&per_page=20&page=1')
+rp('https://seeclickfix.com/api/v2/issues?place_url=hampton&state=VA&per_page=20&page=1')
   .then((data) => {
     const list = JSON.parse(data).issues;
     const summaries = _.map(list, 'summary');
     const response = {
       statusCode: 200,
       body: JSON.stringify({
-      issues: _.union(summaries) || 'There is no issues',
+      issues: _.union(summaries) || 'There are no issues in this city',
       }),
     };
   callback(null, response);
